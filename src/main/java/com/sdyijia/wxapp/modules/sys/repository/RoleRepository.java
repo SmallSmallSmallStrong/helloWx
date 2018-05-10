755d5dc787faba1db9d5aadca9de105528b4265b
shizhen <>
2018-05-10 11:18:32 +0800

    遗漏的dao包

---
+++ src/main/java/com/sdyijia/wxapp/modules/sys/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.sdyijia.wxapp.repository;
+
+import com.sdyijia.wxapp.modules.sys.bean.SysRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<SysRole,Long> {
+
+}
